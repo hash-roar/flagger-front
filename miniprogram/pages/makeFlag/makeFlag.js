651f96d3cd69ec397bdca03c3304a05294bb025d
omittee <88842886+omittee@users.noreply.github.com>
2022-01-22 23:13:29 +0800

    Add files via upload

---
+++ miniprogram/pages/makeFlag/makeFlag.js
@@ -0,0 +1,71 @@
+// pages/makeFlag/makeFlag.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        // sortBig: [{value:"运动",checked:true},{value:"阅读"},{value:"学习"},{value:"其他"}],
+        sortSmall: [{value:'健身',checked:true},{value:'减脂'},{value:'四级'},{value:'六级'},{value:'羽毛球'},{value:'篮球'},{value:'网球'},{value:'足球'},{value:"排球"},{value:"公益"},{value:"乒乓球"},{value:"脱单"},{value:"学习"}],
+        pplNum: [{value:"3 ~ 5",checked:true},{value:"5 ~ 10"},{value:"10+"},{value:"无限制"}],
+        grade: [{value:'大一',checked:true},{value:'大二',checked:true},{value:'大三'},{value:'大四'},{value:'研一'},{value:'研二'},{value:'研三'},{value:'博士生'},],
+        cycle: [{value:'7天',checked:true},{value:'14天'},{value:'21天'},{value:'30天'},{value:'60天'},{value:'90天'},{value:'180天'},{value:'360天'},{value:'自定义'},],
+        frequency: [{value:'1',checked:true},{value:'2'},{value:'3'},{value:'4'},{value:'5'},{value:'6'},{value:'7'},],
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
