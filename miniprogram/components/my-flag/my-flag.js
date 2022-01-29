1ae57e88c199ee58e72912b0e671cf0facebb93a
omittee <88842886+omittee@users.noreply.github.com>
2022-01-29 20:26:32 +0800

    Add files via upload

--- miniprogram/components/my-flag/my-flag.js
+++ miniprogram/components/my-flag/my-flag.js
@@ -15,7 +15,6 @@ Component({
      * 组件的初始数据
      */
     data: {
-        arr:[1,2,3,4,5]
 
     },
 
@@ -26,11 +25,42 @@ Component({
         
         flagDetailFun:function(e){
             wx.navigateTo({
-              url: '/pages/flagDetail/flagDetail',
+              url: '/pages/flagDetail/flagDetail?fid='+this.data.flagObj.fid,
             })
         },
         clockOnFun:function(){
-            console.log('打卡')
+            const token=wx.getStorageSync('token')
+            const p = wx.cloud.callContainer({
+                config: {
+                env: 'prod-6gbc6i9v491283c0', 
+                },
+                path: '/doing-flag',
+                method: 'POST', 
+                data:{
+                    "fid": this.data.flagObj.fid,
+                },
+                header: {
+                    'authentication':token,
+                    'X-WX-SERVICE': 'flagger',
+                    "content-type": "application/json"
+                },
+                success:(res)=>{
+                    if(res.statusCode===200){
+                        wx.showToast({
+                          title: res.data.message,
+                        })
+                    }
+                    else{
+                        wx.showToast({
+                            title: res.data.error,
+                            icon: 'error'
+                        })
+                    }
+                },
+                fail:(res)=>{
+                    console.log(res);
+                }
+            });
         }
     }
 })
