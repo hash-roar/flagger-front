51870b1e75bbf87ff5e964c5383f9a1315aff17e
AlpacaCCC <86586457+AlpacaCCC@users.noreply.github.com>
2022-01-30 13:06:05 +0800

    .

---
+++ miniprogram/components/historyTag/historyTag.js
@@ -0,0 +1,21 @@
+// components/my-flag/my-flag.js
+
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        theCardDetail:{},
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+    },
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+    }
+})
