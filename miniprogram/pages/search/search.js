3bd6d6122031c6b6121d9fcbf253b06c27be8f62
AlpacaCCC <86586457+AlpacaCCC@users.noreply.github.com>
2022-01-30 09:38:08 +0800

    .

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -1,66 +1,27 @@
-// pages/search/search.js
-Page({
-
-    /**
-     * 页面的初始数据
-     */
-    data: {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+// app.js
+App({
+  onLaunch: function () {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力');
+    } else {
+      wx.cloud.init({
+        traceUser: true,
+      });
     }
-})
+
+    this.globalData = {
+      userUID:0,
+      imageURL: "../../images/default-portrait.png",
+      ifIsVistor:"true",
+      firstLogData: {
+        sex:0,
+        grade:0, 
+        major:0,
+        interestedtag:[],
+        createstag:"",
+        environment:0,
+        socialtendency:0,
+      },
+    };
+  },
+});
